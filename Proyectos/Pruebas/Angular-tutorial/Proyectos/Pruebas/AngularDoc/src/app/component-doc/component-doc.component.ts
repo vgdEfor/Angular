4d0821867222fa08d5f34ffd705cec0a017170b1
vgdEfor <vgbodon@efor.es>
2021-04-12 00:10:47 +0200

    Subido angular-tutorial

---
+++ Proyectos/Pruebas/Angular-tutorial/Proyectos/Pruebas/AngularDoc/src/app/component-doc/component-doc.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-component-doc',
+  templateUrl: './component-doc.component.html',
+  styleUrls: ['./component-doc.component.css']
+})
+export class ComponentDocComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+
+}
